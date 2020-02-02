25e9d12aad34c5cd42b0b68fe3d65e5a2b2c55d5
okyeron <1909661+okyeron@users.noreply.github.com>
2020-02-02 15:10:23 -0600

    Clean Up case holes/edges

--- neotrellis_monome_teensy/usb_names.c
+++ neotrellis_monome_teensy/usb_names.c
@@ -7,8 +7,8 @@
 #define MANUFACTURER_NAME  {'m','o','n','o','m','e'}
 #define MANUFACTURER_NAME_LEN 6
 
-// Change this to something different if you like - but keep the "m" as the first character.
-#define SERIAL_NUM  {'m','4','6','7','6','0','0','0'}
+// Change this to something different if you like - but you MUST keep the "m" as the first character.
+#define SERIAL_NUM  {'m','4','6','7','6','0','5','5'}
 #define SERIAL_NUM_LEN 8
 
 struct usb_string_descriptor_struct usb_string_product_name = {
